1a7f349d046f2af966294910b7b2f7fd2a7e3088
tiborkubik <41357737+tiborkubik@users.noreply.github.com>
2019-02-27 07:15:17 +0100

    Add files via upload
    Uploading prototypes for mathematical library

---
+++ src/math_lib.h
@@ -0,0 +1,24 @@
+ 
+//funkcia vracia:
+//	-1, ak je vstup invalid
+//	 inak počet operandov
+int isValidInput(char *expression);
+
+//brano
+double result(char *expression);
+
+double addition(double addend1, double addend2);
+
+double difference(double minuent, double subtrahent);
+
+double multiplication(double multiply1, double multiply2);
+
+double division(double divident, double divisor);
+
+double factorial(double factor);
+
+double powering(double base, double exponent);
+
+double sqrt(double base);
+
+double sine(double base);
