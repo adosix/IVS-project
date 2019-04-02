a2e9bee76dbdc3e35013df5a73e5c57ee1650187
Tibor Kubik <41357737+tiborkubik@users.noreply.github.com>
2019-04-03 01:07:00 +0200

    Functionality of buttons

--- src/RebusCalc/rebuscalc-0.0.1/src/mainwindow.cpp
+++ src/RebusCalc/rebuscalc-0.0.1/src/mainwindow.cpp
@@ -24,20 +24,105 @@ MainWindow::~MainWindow()
    delete ui;
 }
 
-void MainWindow::digit_pressed(){
+void MainWindow::digit_pressed() {
    QPushButton * button = (QPushButton*)sender(); //returns pointer to qObject
 
-   double labelNumber;
-   QString nLabel;
+   //double labelNumber;
+   //QString nLabel;
 
-   labelNumber = (ui->temp_text->text() + button->text()).toDouble(); //reads text from button[digit] and displays it
+   //labelNumber = (ui->temp_text->text() + button->text()); //reads text from button[digit] and displays it
 
-   nLabel = QString::number(labelNumber, 'g', 15);
+   //nLabel = QString::number(labelNumber, 'g', 15);
 
-   ui->temp_text->setText(nLabel);
+   if(ui->temp_text->text() == "0"){
+     ui->temp_text->setText(button->text());
+   }
+   else{
+     ui->temp_text->setText(ui->temp_text->text() + button->text());
+   }
 }
 
-void MainWindow::on_pushButton_comma_released()
-{
+void MainWindow::on_pushButton_comma_released() {
+    ui->temp_text->setText(ui->temp_text->text() + ".");
+}
 
+void MainWindow::on_pushButton_br_left_released() {
+    if(ui->temp_text->text() == "0"){
+        ui->temp_text->setText("(");
+    }
+    else {
+        ui->temp_text->setText(ui->temp_text->text() + "(");
+    }
+}
+
+void MainWindow::on_pushButton_br_right_released() {
+    if(ui->temp_text->text().contains("(")){
+        ui->temp_text->setText(ui->temp_text->text() + ")");
+    }
+}
+
+void MainWindow::on_pushButton_C_released(){
+    ui->temp_text->setText("0");
+}
+
+void MainWindow::on_pushButton_fact_released() {
+    if(ui->temp_text->text() == "0") {
+        return;
+    }
+    ui->temp_text->setText(ui->temp_text->text() + "!");
+}
+
+void MainWindow::on_pushButton_mod_released() {
+    if(ui->temp_text->text() == "0") {
+        return;
+    }
+    ui->temp_text->setText(ui->temp_text->text() + "%");
+}
+
+void MainWindow::on_pushButton_div_released() {
+    if(ui->temp_text->text() == "0") {
+        return;
+    }
+    ui->temp_text->setText(ui->temp_text->text() + "/");
+}
+
+void MainWindow::on_pushButton_mul_released() {
+    if(ui->temp_text->text() == "0") {
+        return;
+    }
+    ui->temp_text->setText(ui->temp_text->text() + "*");
+}
+
+void MainWindow::on_pushButton_plus_released() {
+    if(ui->temp_text->text() == "0") {
+        return;
+    }
+    ui->temp_text->setText(ui->temp_text->text() + "+");
+}
+
+void MainWindow::on_pushButton_minus_released() {
+    if(ui->temp_text->text() == "0") {
+        ui->temp_text->setText("-");
+    }
+    else{
+        ui->temp_text->setText(ui->temp_text->text() + "-");
+    }
+}
+
+void MainWindow::on_pushButton_sin_released() {
+    if(ui->temp_text->text() == "0") {
+        ui->temp_text->setText("sin(");
+    }
+    else{
+        ui->temp_text->setText(ui->temp_text->text() + "sin(");
+    }
+}
+
+void MainWindow::on_pushButton_cos_released() {
+    if(ui->temp_text->text() == "0") {
+        ui->temp_text->setText("cos(");
+    }
+    else{
+        ui->temp_text->setText(ui->temp_text->text() + "cos(");
+    }
 }
--- src/RebusCalc/rebuscalc-0.0.1/src/mainwindow.h
+++ src/RebusCalc/rebuscalc-0.0.1/src/mainwindow.h
@@ -21,6 +21,17 @@ private:
 private slots:
     void digit_pressed();
     void on_pushButton_comma_released();
+    void on_pushButton_br_left_released();
+    void on_pushButton_br_right_released();
+    void on_pushButton_C_released();
+    void on_pushButton_fact_released();
+    void on_pushButton_mod_released();
+    void on_pushButton_div_released();
+    void on_pushButton_mul_released();
+    void on_pushButton_plus_released();
+    void on_pushButton_minus_released();
+    void on_pushButton_sin_released();
+    void on_pushButton_cos_released();
 };
 
 #endif // MAINWINDOW_H
