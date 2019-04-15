a53b138b3c4ce23d63103516c3d36b1301b31285
Branislav Dubec <47944127+BranislavDubec@users.noreply.github.com>
2019-04-15 19:41:34 +0200

    Add files via upload

---
+++ src/RebusCalc/rebuscalc-0.0.1/src/profiling.h
@@ -0,0 +1,8 @@
+#ifndef PROFILING_H
+#define PROFILING_H
+
+double getSmer(double sum,double count,double* sum2);
+double getRandom10();
+double getRandom100();
+double getRandom1000();
+#endif // PROFILING_H
